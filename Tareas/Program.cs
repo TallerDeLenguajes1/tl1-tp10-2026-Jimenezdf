641b9fb2a4c58c79d8bb8181cdbefb8827f642a6
Facundo David Jimenez <fcndojmnz@gmail.com>
2026-06-16 12:37:31 -0300

    ejercicio 1 no funca

---
+++ Tareas/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json;
+
+
+
+HttpClient client = new HttpClient();
+var url = "https://jsonplaceholder.typicode.com/todos/";
+HttpResponseMessage response = await client.GetAsync(url);
+response.EnsureSuccessStatusCode();
+
+string responseBody = await response.Content.ReadAsStringAsync();
+List<Tarea> Tareas = JsonSerializer.Deserialize<List<Tarea>>(responseBody);
+
+foreach(var prod in Tareas)
+{
+    Console.WriteLine("User id: " + prod.UserId + " Id: " + prod.Id + " Titulo: " + prod.Title + " Estado: " + prod.Completed);
+}
---
+++ Tareas/Tarea.cs
@@ -0,0 +1,9 @@
+
+
+public class Tarea
+{
+    public int UserId { get; set; }
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public bool Completed { get; set; }
+}
