d412cd1345964d352a457913332e04656f4e3ad8
Facundo David Jimenez <fcndojmnz@gmail.com>
2026-06-16 13:48:39 -0300

    ejercicio 1

--- Tareas/Program.cs
+++ Tareas/Program.cs
@@ -14,7 +14,26 @@ response.EnsureSuccessStatusCode();
 string responseBody = await response.Content.ReadAsStringAsync();
 List<Tarea> Tareas = JsonSerializer.Deserialize<List<Tarea>>(responseBody, opciones);
 
-foreach(var prod in Tareas)
-{   
-    Console.WriteLine("User id: " + prod.UserId + " Tarea #" + prod.Id + ": " + prod.Title + " (Completada: " + prod.Completed + ")");
+
+List <Tarea> TareasPendientes = Tareas.Where(p => p.Completed == false).ToList();
+List <Tarea> TareasCompletas = Tareas.Where(p => p.Completed == true).ToList();
+
+/*Console.WriteLine("--Tareas Pendientes--");
+foreach (var pend in TareasPendientes)
+{
+    Console.WriteLine("User id: " + pend.UserId + " Tarea #" + pend.Id + ": " + pend.Title + " (Completada: " + pend.Completed + ")");
 }
+
+Console.WriteLine("--Tareas completadas--");
+foreach (var comp in TareasCompletas)
+{
+    Console.WriteLine("User id: " + comp.UserId + " Tarea #" + comp.Id + ": " + comp.Title + " (Completada: " + comp.Completed + ")");
+}
+*/
+
+Console.WriteLine("---- SERIALIZAR ----");
+string JsonList = JsonSerializer.Serialize(Tareas, opciones);
+Console.WriteLine(JsonList);
+
+
+File.WriteAllText("tareas.json", JsonList);
