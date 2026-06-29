d9852733aef8e3217ccefcc7764b01ac6d7e900d
Facundo David Jimenez <fcndojmnz@gmail.com>
2026-06-29 20:12:32 -0300

    ejercicio 3

---
+++ MiWebApi/FotoNasa.cs
@@ -0,0 +1,7 @@
+public class FotoNasa
+{
+    public string Title { get; set; }
+    public string Date { get; set; }
+    public string Explanation { get; set; }
+    public string Url { get; set; }
+}
---
+++ MiWebApi/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.Json;
+
+var opciones = new JsonSerializerOptions
+{
+    WriteIndented = true,
+    PropertyNameCaseInsensitive = true
+};
+
+HttpClient client =  new HttpClient();
+var url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY";
+HttpResponseMessage response = await client.GetAsync(url);
+response.EnsureSuccessStatusCode();
+string responseBody = await response.Content.ReadAsStringAsync();
+
+FotoNasa fotoDelDia = JsonSerializer.Deserialize<FotoNasa>(responseBody,opciones);
+
+Console.WriteLine("=== NASA: FOTO ASTRONÓMICA DEL DÍA ===");
+Console.WriteLine($"Título: {fotoDelDia.Title}");
+Console.WriteLine($"Fecha: {fotoDelDia.Date}");
+Console.WriteLine($"Explicación:\n{fotoDelDia.Explanation}");
+Console.WriteLine($"Enlace a la imagen: {fotoDelDia.Url}");
+string jsonGuardar = JsonSerializer.Serialize(fotoDelDia, opciones);
+File.WriteAllText("nasa_apod.json", jsonGuardar);
+Console.WriteLine("\nArchivo 'nasa_apod.json' guardado con éxito.");
